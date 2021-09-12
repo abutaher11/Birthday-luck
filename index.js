08c8f79932b33b25a32e20ac415bf040a511cb9d
abu.taher11 <taher2world@gmail.com>
2021-09-13 01:28:19 +0530

    added validations

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const dateOfBirth = document.querySelector("#birthday");
 const luckyNumber = document.querySelector("#lucky-num");
 const checkButton = document.querySelector("#check-btn");
+const outputBox = document.querySelector("#output-box");
 
-checkButton.addEventListener('click', function getOutput() {
-    console.log(dateOfBirth.value,luckyNumber.value);
-})
+
+function checkDobIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (dob && luckyNumber.value){
+        compareVal(sum, luckyNumber.value)
+    }
+    else {
+        outputBox.innerText = "Please enter both fields to continue";
+    }
+    
+}
+
+function compareVal(sum, luckyNumber){
+    if(sum%luckyNumber==0){
+        outputBox.innerText = "Your birthday is lucky";
+    }
+    else {
+        outputBox.innerText = "Your make your own luck";
+    }
+}
+
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let i=0; i<=dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+checkButton.addEventListener('click', checkDobIsLucky)
